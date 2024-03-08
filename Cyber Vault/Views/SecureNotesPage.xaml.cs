9126b3d73271d9dfdcf29cf20447cf997addada1
affan-ch <muhammadaffanmaqsood@gmail.com>
2024-03-08 23:26:40 +0500

    Add Secure Note Page UI
    Co-Authored-By: MUHAMMAD FARMAN <98874196+MUHAMMAD-FARMAN@users.noreply.github.com>

--- Cyber Vault/Views/SecureNotesPage.xaml.cs
+++ Cyber Vault/Views/SecureNotesPage.xaml.cs
@@ -1,11 +1,19 @@
 ﻿using Cyber_Vault.ViewModels;
-
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Windows.Storage.Pickers;
+using Windows.Storage.Provider;
+using Windows.Storage;
+using Microsoft.UI.Text;
+using Windows.UI;
 
 namespace Cyber_Vault.Views;
 
 public sealed partial class SecureNotesPage : Page
 {
+    private Color currentColor;
+
     public SecureNotesViewModel ViewModel
     {
         get;
@@ -15,5 +23,74 @@ public sealed partial class SecureNotesPage : Page
     {
         ViewModel = App.GetService<SecureNotesViewModel>();
         InitializeComponent();
+    }
+
+    private void AddSecureNote_Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+
+    }
+
+    private void SearchBar_KeyUp(object sender, Microsoft.UI.Xaml.Input.KeyRoutedEventArgs e)
+    {
+
+    }
+
+
+    private void TitleInfo_Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        Category_TeachingTip.IsOpen = false;
+        Title_TeachingTip.IsOpen = true;
+    }
+
+    private void CategoryInfo_Button_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        Title_TeachingTip.IsOpen = false;
+        Category_TeachingTip.IsOpen = true;
+    }
+
+    private void Category_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (CustomCategory_Container != null)
+        {
+            if (Category_ComboBox.SelectedIndex >= 0)
+            {
+                if (Category_ComboBox.SelectedValue.ToString() == "Custom")
+                {
+                    CustomCategory_Container.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    CustomCategory_Container.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+
+    }
+
+
+
+
+    private void Editor_GotFocus(object sender, RoutedEventArgs e)
+    {
+        editor.Document.GetText(TextGetOptions.UseCrlf, out _);
+
+        // reset colors to correct defaults for Focused state
+        var documentRange = editor.Document.GetRange(0, TextConstants.MaxUnitCount);
+        var background = (SolidColorBrush)App.Current.Resources["TextControlBackgroundFocused"];
+
+        if (background != null)
+        {
+            documentRange.CharacterFormat.BackgroundColor = background.Color;
+        }
+    }
+
+    private void Save_Button_Click(object sender, RoutedEventArgs e)
+    {
+
+    }
+
+    private void CustomCategory_Button_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
